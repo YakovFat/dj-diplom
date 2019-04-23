9187600c169626d16fe1d2938250ae58c379d738
Yakov Fat <YakovFat@yandex.ru>
2019-04-23 22:28:38 +0300

    незаконченные модели

--- myshop/clientapp/models.py
+++ myshop/clientapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Phone(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=100)
+    slug = models.SlugField()
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.ManyToManyField('Phone')
--- myshop/clientapp/urls.py
+++ myshop/clientapp/urls.py
@@ -3,10 +3,11 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [ 
-    path('cart/', view_cart, name='cart'),
+    path('', view_index, name='index'),
     path('empty_section/', view_empty_section, name='empty_section'),
-    path('index/', view_index, name='index'),
-    path('login/', view_login, name='login'),
     path('phone/', view_phone, name='phone'),
     path('smartphones/', view_smartphones, name='smartphones'),
+    path('cart/', view_cart, name='cart'),
+    path('login/', view_login, name='login'),
+    path('smartphones/<str>', view_smartphones, name='smartphones'),
 ]
